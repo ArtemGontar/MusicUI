fb2af4dcc3537e0c721e12f6703f474848657ecc
art.hontar <art.hontar@andersenlab.com>
2019-06-28 13:53:56 +0300

    add custom player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SuggestionBoxComponent } from './suggestion-box/suggestion-box.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SuggestionBoxComponent
+    SuggestionBoxComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
